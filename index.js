1aa70308fc622be87280957afc4f2e4ace93a506
klaudiaciba <117776759+klaudiaciba@users.noreply.github.com>
2023-03-11 12:29:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
-
 var image1 = document.querySelectorAll("img") [0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6 ) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
 
